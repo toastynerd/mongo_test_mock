c98fe940823751f6dfe8e89bc1c77e8e8d8f3f82
toastynerd <tylermorgan86@gmail.com>
2015-07-31 10:53:50 -0700

    working on better docs for wire protocol and other stuff

---
+++ helpers/test_server_response.js
@@ -0,0 +1,15 @@
+var net = require('net');
+var request = new Buffer('OAAAAAAAAAAAAAAA1AcAAAAAAABzeXN0ZW0uJGNtZAAAAAAA/////xAAAAAIaXNtYXN0ZXIAAQA=', 'base64');
+var BSON = require('bson').BSONPure.BSON;
+var bson = new BSON();
+
+var socket = net.createConnection(27017);
+socket.on('data', function(data) {
+  console.log(bson.deserialize(data.slice(36, 194)));
+});
+
+socket.on('end', function() {
+  console.log('disconnected');
+});
+
+socket.write(request);
